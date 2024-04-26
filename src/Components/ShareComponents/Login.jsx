39261caded1e74d5841d8002e4572b66b2f08d81
MdJowelAhmed <155435567+MdJowelAhmed@users.noreply.github.com>
2024-04-26 10:35:46 +0600

    added register page

--- src/Components/ShareComponents/Login.jsx
+++ src/Components/ShareComponents/Login.jsx
@@ -1,12 +1,134 @@
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import { useContext, useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+// import { ToastContainer, toast } from 'react-toastify';
+import Swal from 'sweetalert2'
+import { AuthContext } from "../Provider/AuthProvider";
+
 
 
 const Login = () => {
+    const { loginUser} = useContext(AuthContext)
+    const navigate = useNavigate()
+    const location = useLocation()
+    const [loginError, setLoginError] = useState('')
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = data => {
+
+        // console.log(data)
+        const { email, password } = data;
+        if (email.length < 0) {
+            setLoginError('please email ')
+            return
+        }
+        if (password.length < 0) {
+            setLoginError('password min 6 letter')
+            return
+        }
+        // console.log(data)
+        setLoginError('')
+        loginUser(email, password)
+
+            .then(result => {
+                if (result.user) {
+                    Swal.fire({
+                        title: "Login success!",
+                        text: "You clicked the button!",
+                        icon: "success"
+                    });
+                    navigate(location?.state || "/")
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                setLoginError(error.message)
+            })
+    }
+    const handleSocialLogin = socialProvider => {
+        socialProvider()
+            .then(result => {
+                if (result.user) {
+                    Swal.fire({
+                        title: "Good job!",
+                        text: "You clicked the button!",
+                        icon: "success"
+                    });
+                    navigate(location?.state || "/")
+                }
+            })
+            .catch(error => {
+                console.log(error)
+
+            })
+    }
     return (
         <div>
             <Navbar></Navbar>
-            login
+            <div>
+                <div>
+                
+                    <div className="hero min-h-screen bg-base-200">
+                        <div className="hero-content flex-col">
+                            <div className="text-center ">
+                                <h1 className="text-5xl font-bold">Login now!</h1>
+
+                            </div>
+                            <div className="card shrink-0  lg:min-w-96  shadow-2xl bg-base-100">
+                                <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Email</span>
+                                        </label>
+                                        <input type="email" placeholder="email" className="input input-bordered" {...register("email", { required: true })} />
+                                        {errors.email && <span className="text-red-600 text-sm">This field is required</span>}
+                                    </div>
+                                    <div className="form-control">
+                                        <label className="label">
+                                            <span className="label-text">Password</span>
+                                        </label>
+                                        <input type="password" placeholder="password" className="input input-bordered" {...register("password", { required: true })} />
+                                        {errors.password && <span className="text-red-600 text-sm">{errors.Password.message}</span>}
+                                        <label className="label">
+                                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                        </label>
+                                    </div>
+                                    <div className="form-control mt-6">
+                                        <button className="btn btn-primary">Login</button>
+                                        {/* <ToastContainer /> */}
+                                    </div>
+                                </form>
+                                {
+                                    loginError && <span className="text-red-500 text-center mb-3">{loginError} </span>
+                                }
+                                <div className="text-center mb-5">
+                                    <p>Did not have account? please <span><Link to='/register' className="text-blue-600 font-semibold">Register</Link></span></p>
+                                </div>
+                                <div className="divider divider-accent">Login With</div>
+                                <div className="mx-5 mb-5 flex justify-between">
+                                    <div>
+
+                                        <button onClick={() => handleSocialLogin(googleLogin)} className="btn btn-accent"> <FaGoogle /> Google</button>
+                                    </div>
+                                    <button onClick={() => handleSocialLogin(githubLogin)} className="btn btn-accent"><FaGithub /> Github</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/Components/ShareComponents/Register.jsx
+++ src/Components/ShareComponents/Register.jsx
@@ -1,12 +1,139 @@
 import Footer from "./Footer/Footer";
 import Navbar from "./Navbar";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa"
+import { useContext, useState } from "react";
+import Swal from 'sweetalert2'
+import { AuthContext } from "../Provider/AuthProvider";
 
 
 const Register = () => {
+    const [showPassword,setShowPassword]=useState(false)
+    const [regError,setRegError]=useState('')
+    const [success,setSuccess]=useState('')
+
+    const {createUser}=useContext(AuthContext)
+    const navigate=useNavigate()
+    const location=useLocation()
+    // console.log(createUser)
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = data => {
+       
+        // console.log(data)
+        const { email, password} = data;
+        // console.log(email,password)
+        setRegError('')
+        createUser(email,password)
+        .then(result=>{
+            // updateProfile(name,image)
+           if(result.user){
+            Swal.fire({
+                title: "Register successFull!",
+                text: "You clicked the button!",
+                icon: "success"
+              });
+            navigate(location?.state || "/")
+            setSuccess('register success')
+           }
+                
+         })
+        .catch(error=>{
+            console.log(error)
+            setRegError(error.message)
+        })
+    }
     return (
         <div>
             <Navbar></Navbar>
-            register
+            {/* register */}
+        
+            <div>
+            
+           
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center ">
+                        <h1 className="text-5xl font-bold">Register First!</h1>
+
+                    </div>
+                    <div className="card shrink-0  lg:min-w-96  shadow-2xl bg-base-100">
+                        <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Your Name</span>
+                                </label>
+                                <input type="text" name="Name" placeholder="Your Name" className="input input-bordered" {...register("Name", { required: true })} />
+                                {errors.Name && <span className="text-red-600 text-sm">This field is required</span>}
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name="Email" placeholder="Your Email" className="input input-bordered" {...register("email", { required: true })} />
+                                {errors.email && <span className="text-red-600 text-sm">This field is required</span>}
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Image URL</span>
+                                </label>
+                                <input type="text" name="image" placeholder="Image URL" className="input input-bordered" {...register("image",{ required: true })}/>
+                                {errors.email && <span className="text-red-600 text-sm">This field is required</span>}
+                            </div>
+                            <div className="form-control relative">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type={showPassword ?"text" :"password"} name="Password" placeholder="password" className="input input-bordered" {...register("password", {
+                                    required: {
+                                        value: true,
+                                        message: "You must fill the password field"
+                                    },
+                                    minLength: {
+                                        value: 6,
+                                        message: 'Password must min 6 characters'
+                                    },
+                                    pattern: {
+                                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/,
+                                       
+                                        // value: '[A-Z]',
+                                        message: 'Uppercase and lowerCase letter also number added '
+                                    }
+                                })} />
+                                {errors.password && <span className="text-red-600 text-sm">{errors.password.message}</span>}
+                                <div onClick={()=>setShowPassword(!showPassword)} className="absolute ml-72 mt-12">
+                                    {
+                                       showPassword ?  <FaEye /> :  <FaEyeSlash />
+                                    }
+                                   
+                                   
+                                </div>
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Register</button>
+                             
+                            </div>
+                        </form>
+                        {
+                            regError && <span className="text-red-600 text-center mb-3">{regError} </span>
+                        }
+                        {
+                            success && <span>{toast.success('successfully')} </span>
+                        }
+                        <div className="text-center mb-5">
+                            <p>Have you account? please <span><Link to='/login' className="text-blue-600 font-semibold">Login</Link></span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+        </div>
             <Footer></Footer>
         </div>
     );
